7066099d38ca0e59ca07c15ad7cc72116e0cd967
farrajota <mlgfarrajota@outlook.com>
2017-04-21 15:28:05 +0100

    small fix

--- roi_proposals/script_process_rois_all.m
+++ roi_proposals/script_process_rois_all.m
@@ -1,20 +1,22 @@
 function script_process_rois_all(data_dir, save_dir)
 %% initializations
-algs = ['acf', 'ldcf', 'edgeboxes'];
+algs = {'acf', 'ldcf', 'edgeboxes'};
 
 %% Process rois
 for i=1:1:size(algs,1)
+    algorithm = algs{i};
+    
     %% caltech
-    script_process_rois_caltech(data_dir, save_dir, algs(i))
+    script_process_rois_caltech(data_dir, save_dir, algorithm);
     
     %% inria
-    script_process_rois_inria(data_dir, save_dir, algs(i))
+    script_process_rois_inria(data_dir, save_dir, algorithm);
 
     %% eth
-    script_process_rois_eth(data_dir, save_dir, algs(i))
+    script_process_rois_eth(data_dir, save_dir, algorithm);
 
     %% tudbrussels
-    script_process_rois_tudbrussels(data_dir, save_dir, algs(i))
+    script_process_rois_tudbrussels(data_dir, save_dir, algorithm);
 end
 
 end
