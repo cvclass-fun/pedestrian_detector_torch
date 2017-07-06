8831e0dd6b26b8f5fbab8e9d9c78c29b62f75912
farrajota <mglfarrajota@outlook.com>
2017-07-06 10:32:27 +0100

    Fix dbcollection require

--- data.lua
+++ data.lua
@@ -4,7 +4,7 @@
 
 
 local function get_db_loader(name)
-    local dbc = require 'dbcollection.manager'
+    local dbc = require 'dbcollection'
 
     local dbloader
     local str = string.lower(name)
